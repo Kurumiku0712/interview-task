fb25540e79f2cba1f75b15db3bf1947f257f59fc
Kurumiku0712 <kurumiku0712@gmail.com>
2025-02-27 14:07:26 +1100

    feat: Setup toolkit saga

--- Q3/tree-select-app/src/App.jsx
+++ Q3/tree-select-app/src/App.jsx
@@ -1,49 +1,9 @@
-// src/App.jsx
-import { useSelector, useDispatch } from 'react-redux';
-import { fetchCategoriesSaga } from './redux/saga';
-import { useFetchCategories } from './hooks/useFetchCategories';
-import CategoryTreeSelect from './components/CategoryTreeSelect';
 
-function App() {
-  const dispatch = useDispatch();
-  const categories = useSelector((state) => state.categories);
-  const { fetchData: hookFetch } = useFetchCategories();
 
-  const handleSelect = (categoryId) => {
-    alert(`Selected Category ID: ${categoryId}`);
-  };
-
+const App = () => {
   return (
-    <div style={{ padding: '20px' }}>
-      <h2>Category TreeSelect Demo</h2>
-
-      {/* Button 1: Use Redux Saga */}
-      <button
-        onClick={() => {
-          dispatch(fetchCategoriesSaga());
-        }}
-      >
-        Fetch via Redux Saga
-      </button>
-
-      {/* Button 2: Use Custom Hook */}
-      <button
-        onClick={() => {
-          hookFetch();
-        }}
-        style={{ marginLeft: '10px' }}
-      >
-        Fetch via Custom Hook
-      </button>
-
-      <div style={{ marginTop: '20px' }}>
-        <CategoryTreeSelect
-          categories={categories}
-          onSelect={handleSelect}
-        />
-      </div>
-    </div>
-  );
+    <div>App</div>
+  )
 }
 
-export default App;
+export default App
